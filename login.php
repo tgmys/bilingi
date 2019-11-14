ffa172e0db6632448ebbb1e41f93386b632a7ff2
tgmys <t.gmys@o2.pl>
2019-11-14 12:00:12 +0100

    login

--- login.php
+++ login.php
@@ -1,49 +1,33 @@
 <?php
 
-//$config=require_once 'config.php';
-//require_once 'Connectdb.php';
 require_once './database.php';
-try{
-  //  $db= new PDO($config['host'], $config['user'], $config['password']);
-    $strt= $db ->query("select imie, nazwisko, haslo from uzytkownicy");
-    
+//require_once './index.html';
+/*
     $tmp = $strt->fetch(PDO::FETCH_ASSOC);
     var_dump($tmp);
     $strt->closeCursor();
+*/
 
-//echo $strt ->rowCount();
-    
-   // $takie = $db;
-    
-} catch (Exception $ex) {
-    echo 'sd';
-}
 
-//$db=new Connectdb();
-//$db= $db ->getConnection();
+//$sr= $db ->query("select* from uzytkownicy")  ;
+//var_dump($sr);
 
-$sr= $db ->query("select* from uzytkownicy")  ;
-var_dump($sr);
-/*
 $em=$_POST['email'];
 $pas=$_POST['pass'];
-if($pas=='kowal')
-    echo 'ok';
-if($em=='jkowal@o2.pl')
-    echo 'ok1';
-
 
 if (isset($_POST['email']) && isset($_POST['pass'])){
-    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    
-    
-   $st = $db->prepare("select * from uzytkownicy where haslo=?");
-        //$st->bindParam(1, $em);
-        $st->bindParam(1, $pas);
+   // $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+   $st = $db->prepare("select * from uzytkownicy where email = ? and haslo =?;");
+                       
+        $st->bindParam(1, $em);
+        $st->bindParam(2, $pas);
         
         $st->execute();
+        $tmp = $st->fetchAll();
+      
+        var_dump($tmp);
         
-        if($st->rowCount() > 0){
+        if(count($tmp) > 0){
             echo "User verified, Access granted.";
         } else {
             echo "Incorrect username or password";
@@ -52,7 +36,7 @@ if (isset($_POST['email']) && isset($_POST['pass'])){
     header('Location: index.html');
     exit();
     
-}*/
+}
 
 ?>
 
