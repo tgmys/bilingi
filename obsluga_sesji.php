684caf62008737e942eb5a7bc94ec300a38497c7
tgmys <t.gmys@o2.pl>
2019-12-01 23:50:03 +0100

    dodanie obslugi sesji

---
+++ obsluga_sesji.php
@@ -0,0 +1,17 @@
+<?php
+
+session_start();
+
+if(!isset($_SESSION['zalogowany']))
+{
+    session_regenerate_id();
+    
+    $_SESSION['zalogowany'] = true;
+    $_SESSION['ip'] = $_SERVER['REMOTE_ADDR'];
+}
+
+if($_SESSION['ip'] != $_SERVER['REMOTE_ADDR'])
+{
+    die('Proba przejecia sesji udaremniona!');
+}
+?>
