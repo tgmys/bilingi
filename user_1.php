2e7362967dc82dc308bf7f73eb82ee230c732f50
tgmys <t.gmys@o2.pl>
2019-12-14 21:14:01 +0100

    wyswietlanie bilingow

--- user_1.php
+++ user_1.php
@@ -4,13 +4,18 @@ include_once 'header.php';
 require_once './database.php';
 $id_us=$_SESSION['id_uz'];
 $user= $db->query("select imie,nazwisko from uzytkownicy where id_uz='$id_us';"); 
-$num=$db->query("select data, godzina_polaczenia, czas_trwania_polaczenia, polaczenia.numer_telefonu tr , nr_tel.numer_telefonu as rt "
-        . "from uzytkownicy inner join nr_tel on nr_tel.id_uz=uzytkownicy.id_uz left join polaczenia on nr_tel.id_tel=polaczenia.id_tel  where uzytkownicy.id_uz='$id_us';
+$num=$db->query("select id_tel, numer_telefonu rt from  nr_tel  where id_uz='$id_us';
+");
+$bili=$db->query("select kierunek_polaczenia, data, godzina_polaczenia, czas_trwania_polaczenia, polaczenia.numer_telefonu tr , nr_tel.numer_telefonu as rt "
+        . "from  nr_tel left join polaczenia on nr_tel.id_tel=polaczenia.id_tel  where id_uz='$id_us';
 ");
 //$num=$num->fetch(PDO::FETCH_ASSOC);
+$tel_ids=$num->fetch(PDO::FETCH_ASSOC);
+$tel_id=$tel_ids['id_tel'];
 $user=$user->fetch(PDO::FETCH_ASSOC);
 //$bili=$db->query("select ");
 //var_dump($num);
+$num->execute();
 ?> 
 
 <body >
@@ -42,16 +47,25 @@ $user=$user->fetch(PDO::FETCH_ASSOC);
     <div class="container">
         <h3> Numery telefonów : </h3>
         <ul class="nav navbar-nav navbar-left">
-          <?php $num1=array(); echo ' <li>'; foreach ($num as $row1) { if($row1['rt']!=$num1){ echo $row1['rt'], ' '; $num1=$row1['rt'];}} echo' </li>';var_dump($num1); ?>
+            
+          <?php 
+          $num1=array(); 
+          foreach ($num as $row1)
+              { 
+              if($row1['rt']!=$num1)
+                  { 
+                  echo ' <li ><a>';
+                  echo $row1['rt'], ' ';
+                  
+                  $num1=$row1['rt'];}
+              echo'</a> </li>';
+              }
+              ?>
     </ul>
     </div>
 </nav>
  <div class="row">
 	<h1>
- </div>	
-	
- <div class="row">
-	<h1>wiersz 2
  </div>	
 	
 <div class="row">
@@ -65,21 +79,24 @@ $user=$user->fetch(PDO::FETCH_ASSOC);
 		  <thead>
 			<tr>
                         
-				<th>Imię</th>
-				<th>Nazwisko</th>
-				<th>Nr telefonu </th>
+				<th>kierunek_polaczenia</th>
+				<th>data</th>
+				<th>godzina_polaczenia </th>
+                                <th>czas_trwania_polaczenia </th>
+                                <th>numer telefonu </th>
 			</tr>
 		  </thead>';
 	
 	echo '<tbody>';
 	
-for($i=1; $i<10; $i++)
+        foreach ($bili as $row2)
 	{
 		echo '<tr>
-				<td>'. $_POST['imie'] .'</td> 
-				<td>'. $_POST['nazwisko'] .'</td>
-				<td>'. $_POST['nr_telefonu'] .'</td>
-				
+				<td>'. $row2['kierunek_polaczenia'] .'</td> 
+				<td>'. $row2['data'] .'</td>
+				<td>'. $row2['godzina_polaczenia'] .'</td>
+                                <td>'. $row2['czas_trwania_polaczenia'] .'</td>
+				<td>'. $row2['tr'] .'</td>
 			</tr>';
 	}
     echo   '</tbody>
