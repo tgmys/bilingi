938fca323fd2bb4021edd48a7fdf475ae9a400aa
tgmys <t.gmys@o2.pl>
2020-02-13 11:58:44 +0100

    przepisanie numeru do abonenta poprawki

--- przypis.php
+++ przypis.php
@@ -55,63 +55,45 @@
 	
 	
   // Akcja przypisywania zaznaczonych rekordów (unikatowych) do tabeli polacz	//Post tabela z unikatowymi numerów abonenta
-  
-                
-//	if ($ile1 > 0 && $ile2 > 0 ) 
-//	{	
-//		for ($i=1; $i<=$ile1; $i++)
-//		{
-//			$pom = $_POST['nazwa'][$i-1];
-//			
-//			try
-//			{
-//				$add = $db -> 	query	("INSERT INTO polacz
-//										(id_tel, id_bili)
-//										VALUES 
-//										('$ile2', '$pom');
-//										");
-//			}
-//	
-//			catch (Exception $e)
-//			{
-//				$komunikat.= 'Błąd dla bilingu:  ' . $pom . ' <br />';
-//			}	
-//			
-//		}
-//	}
+          if(isset($_POST['nazwa']) && isset($_POST['wybor_abonenta']) )
+          {
+              $a = 0;
+              $str="";
+  foreach ($_POST['nazwa'] as $p)
+  {
+
+      try
+			{
+				 $db ->  query	("INSERT INTO polacz
+										(id_tel, id_bili)
+										VALUES 
+										('$_POST[wybor_abonenta]', '$p');
+										");
+			}
 	
-  // Tworzenie tabeli pomocniczej - "bilingi[]" - z aktualnymi rekordami id_bili
-	$a = 0;
-	$lista_bili = $db -> query('select id_bili from polacz;');
-	foreach($lista_bili as $b)
-	  {
-		$bilingi[$a] = $b[0];
+			catch (Exception $e)
+			{
+				$komunikat.= 'Błąd dla bilingu:  ' . $p . ' <br />';
+			}	
+	// Tworzenie tabeli pomocniczej - "bilingi[]" - z aktualnymi rekordami id_bili
+               $bilingi[$a] = $p;
 		$a++;
-      }
-  // KONIEC tworzenie tabeli pomocniczej - "bilingi[]" - z rekordami id_bili
-  
-  //Spr bilingu w tabeli nie_przyp i przepisanie pola do polaczenia i kasacja w nie_przyp
-	
-	$lista_polaczenia = $db -> query('select  id_bili, data, godzina_polaczenia, czas_trwania_polaczenia, kierunek_polaczenia, numer_telefonu from nie_przyp;');
-	
-	foreach($lista_polaczenia as $p)
+  } 
+ 
+
+$qMarks = str_repeat('?,', count($bilingi) - 1) . '?';
+$sth = $db->prepare("SELECT * FROM nie_przyp WHERE id_bili IN ($qMarks)");
+$sth->execute($bilingi);
+
+	foreach($sth as $p)
 	  {
-		//if (in_array($p[1], $bilingi)) 
-		{
 			try
 			{
-		
 				$add1 = $db -> query	("INSERT INTO polaczenia
 							(id_bili, data, godzina_polaczenia, czas_trwania_polaczenia, kierunek_polaczenia, numer_telefonu)
 							VALUES 
-							('$p[1]', '$p[2]', '$p[3]', '$p[4]', '$p[5]', '$p[6]');
-							");
-				
-				if($add1)
-				{
-					$kasuj = $db -> query ("DELETE FROM nie_przyp WHERE id_p='$p[0]'");
-				}
-			
+							('$p[0]', '$p[1]', '$p[2]', '$p[3]', '$p[4]', '$p[5]');
+							");											
 			}
 	
 			catch (Exception $e)
@@ -120,12 +102,19 @@
 				$komunikat.= 'Błąd w wierszu:  ' . $wiersz . '::' . $id_bilingu. ' '. $data. ' '.$godzina. ' ' .$czas. ' '. $rozmowa .' ' .$nr_telefonu.' <br />';
 			}
 		
-		}
+		
       }
+      if($add1)
+	{
+         $qMarks = str_repeat('?,', count($bilingi) - 1) . '?';
+	$kasuj= $db -> prepare ("DELETE FROM nie_przyp  WHERE id_bili IN ($qMarks);");
+   
+        $kasuj->execute($bilingi);
+        }
 	
 	
-	$lista_polaczenia->closeCursor();
-	$lista_bili->closeCursor();
+	$sth->closeCursor();
+	}
 	echo '</div></div class="row">';
   	echo '<div class="row"> </div> <div class="col-lg-2">  </div> <div class="col-lg-2">';
 
@@ -134,7 +123,7 @@
 
   // Formularz z polem select - dane z tabeli nie_przyp
        
-	echo '<form class="form-inline" role="form" method="POST" action="przypis.php"> 
+	echo '<form class="form-inline" role="form" method="POST" action="testy.php"> 
 		<select id= "se" multiple name="nazwa[]" size="15">';
 		
 		foreach ($lista_nieprzyp as $linia)
